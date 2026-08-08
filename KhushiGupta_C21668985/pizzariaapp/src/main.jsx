494f7d5dec5d2361bbf8bb2fa7d989882769b2e3
Khushi Gupta <151847965+Khushi1620@users.noreply.github.com>
2026-08-08 15:14:59 +0530

    Update main.jsx

--- KhushiGupta_C21668985/pizzariaapp/src/main.jsx
+++ KhushiGupta_C21668985/pizzariaapp/src/main.jsx
@@ -10,6 +10,7 @@ import { store } from './redux/store.js';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* We can use redux in each component in app that's why app is running inside the provider  */}
+    {/* store(key which can't be changed just like className) = {store(name of the variable which you written in redux store)} */}
     <Provider store={store}>
        {/* calling a function */}
       <App />
